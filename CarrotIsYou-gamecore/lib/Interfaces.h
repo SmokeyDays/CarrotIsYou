d64aa4d665b49544ffab51d2e8db94a4990d856f
SmokeyDays <fyq61002427@126.com>
2023-10-17 18:22:27 +0800

    feat: added keys

--- CarrotIsYou-gamecore/lib/Interfaces.h
+++ CarrotIsYou-gamecore/lib/Interfaces.h
@@ -1,13 +1,13 @@
 #pragma once
 #define CARROT_ON_SDL
 #ifdef CARROT_ON_SDL
-  #define KEY_UP 0x40000052
-  #define KEY_DOWN 0x40000051
-  #define KEY_LEFT 0x40000050
+  #define KEY_UP    0x40000052
+  #define KEY_DOWN  0x40000051
+  #define KEY_LEFT  0x40000050
   #define KEY_RIGHT 0x4000004F
-  #define KEY_KEEP 9
-  #define KEY_UNDO 4
-  #define KEY_RESTART 5
+  #define KEY_KEEP 0x20
+  #define KEY_UNDO 0x7A
+  #define KEY_RESTART 0x72
   #define KEY_ENTER 6
   #define KEY_BACK 7
   #define KEY_NEXT 8
