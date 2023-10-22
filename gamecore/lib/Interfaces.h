e352be147a294087cd9168fa54118cf18cad5b61
SmokeyDays <fyq61002427@126.com>
2023-10-22 17:11:54 +0800

    fixed: define riscv

--- gamecore/lib/Interfaces.h
+++ gamecore/lib/Interfaces.h
@@ -1,5 +1,5 @@
 #pragma once
-#define CARROT_ON_SDL
+#define RISCV
 #ifdef CARROT_ON_SDL
   #include<cstdio>
   #define KEY_UP    0x40000052
@@ -18,7 +18,21 @@
   #define KEY_P 0x70
 #endif
 #ifdef RISCV
+
+  #define KEY_UP    0x40000052
+  #define KEY_DOWN  0x40000051
+  #define KEY_LEFT  0x40000050
+  #define KEY_RIGHT 0x4000004F
+  #define KEY_W     0x77
+  #define KEY_S     0x73
+  #define KEY_A     0x61
+  #define KEY_D     0x64
   #define KEY_SPACE 0x29
+  #define KEY_Z 0x7A
+  #define KEY_R 0x72
+  #define KEY_ENTER 0xD
+  #define KEY_Q 0x71
+  #define KEY_P 0x70
 #endif
 
 extern void sdlSetDisplayMemory(int x, int y, unsigned int value);
